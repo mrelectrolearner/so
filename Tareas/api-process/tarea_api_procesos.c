c13baca6de4f6264d411cfda1193a15f47601a1f
mrelectrolearner <78055368+mrelectrolearner@users.noreply.github.com>
2021-12-27 17:25:26 -0500

    Update tarea_api_procesos.c

--- Tareas/api-process/tarea_api_procesos.c
+++ Tareas/api-process/tarea_api_procesos.c
@@ -4,28 +4,27 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-//#include "leercadena.h"
 
 int main(int argc, char *argv[]) {
-    //char **vector;
+   
     char *comando=argv[1];
     char *argument=argv[2];
-    //int i;
+    
     pid_t rc;
-    printf("*:%d, not*:%s",*comando, comando);
-    if(argc==1){
-        printf("argc: %d, comando: %s, argv:%s \n",argc, comando,*argv);
+   
+    if(argc==1||argc>3){
+        
         printf("Indique el programa que se desea ejecutar \n");
         exit(1);
     }
     rc = fork();
-    printf("pid:%d",rc);
+    
     if (rc < 0) {
-      //perror("No se pudo crear un proceso\n");
+      
       exit(1);
     } else if (rc == 0) {
       execlp(comando, comando,argument,NULL);
-      //perror("Return from execlp() not expected"); exit(EXIT_FAILURE);
+     
     } else {
       wait(NULL);
  }
