e864f656c52bd3639815fa061743778dcf6573d6
luis <luisfelorivas@gmail.com>
2022-01-19 23:26:01 -0500

    add pipe3.c

---
+++ Tareas/pipe/pipe3.c
@@ -0,0 +1,62 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <time.h>
+#define READ  0
+#define WRITE 1
+int rdm();
+int main(void) {
+  pid_t pidC;
+  int   fd[2];
+  char buffer_read[10];
+  char buffer_write[10];
+  if (pipe(fd) == -1) {
+	perror("Creating pipe");
+	exit(EXIT_FAILURE);
+  }
+  pidC = fork();
+  switch(pidC) {
+
+  case 0:
+	// The child process will execute wc.
+	// Close the pipe write descriptor.
+	close(fd[WRITE]);
+	// Redirect STDIN to read from the pipe.
+	int y;
+	read(fd[READ],&y, sizeof(int));
+	
+	printf("numero %d  \n",y);
+	close(fd[READ]);
+	
+	break;
+
+  case -1:
+	perror("fork() failed");
+	exit(EXIT_FAILURE);
+	break;
+
+  default:
+	// The parent process will execute ls.
+	// Close the pipe read descriptor.
+	close(fd[READ]);
+	// Redirect STDOUT to write to the pipe.
+	
+    	//sprintf(buffer_write, "%d",50);
+    	int x=rdm();
+	write(fd[WRITE], &x, sizeof(int));
+	close(fd[WRITE]);
+	//dup2(fd[WRITE], STDOUT_FILENO);
+	// Execute ls -l
+	exit(0);
+	break;
+
+}
+}
+
+int rdm(){
+	srand(time(NULL));
+	int random_number = rand() % 1000 + 1;
+	return random_number;
+}
